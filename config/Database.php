9878f19ed5ede3eb6e1d4cf914f364fea9025ea1
Juan Carlos Gonzalez <150765215+CarlosGonzalez7u7@users.noreply.github.com>
2025-10-10 23:14:11 -0600

    Update Database.php, cambie para comunicacion a la BD remota

--- config/Database.php
+++ config/Database.php
@@ -2,8 +2,8 @@
 
 class Database {
     private $host = "localhost";
-    private $user = "root";
-    private $password = "12345678";
+    private $user = "juancarlos";
+    private $password = "juancarlos";
     private $database = "webnahim";
     private $connection;
     
